1736c0ba7e09681fb326c15d6ddb8d724c05a75c
8471919 <7471919@naver.com>
2021-09-10 19:20:23 +0900

    feat: createUserDto 추가 #0

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUserDto {}
+import { PickType } from '@nestjs/mapped-types';
+import { Users } from '../entities/user.entity';
+
+export class CreateUserDto extends PickType(Users, [
+  'email',
+  'password',
+] as const) {}
