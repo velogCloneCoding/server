c3df0bebdbf9aacbf8f7beba126f3764a905599a
8471919 <7471919@naver.com>
2021-11-07 18:51:38 +0900

    setting:개발환경을 위한 cross-env 세팅 #0

--- ormconfig.ts
+++ ormconfig.ts
@@ -9,11 +9,26 @@ dotenv.config();
 
 const ORMConfig: any & ConnectionOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host:
+    process.env.NODE_ENV === 'development'
+      ? process.env.DEVELOPMENT_DB_HOST
+      : process.env.TEST_DB_HOST || process.env.DB_HOST,
+  port:
+    process.env.NODE_ENV === 'development'
+      ? process.env.DEVELOPMENT_DB_PORT
+      : Number(process.env.DB_PORT),
+  username:
+    process.env.NODE_ENV === 'development'
+      ? process.env.DEVELOPMENT_DB_USERNAME
+      : process.env.DB_USERNAME,
+  password:
+    process.env.NODE_ENV === 'development'
+      ? process.env.DEVELOPMENT_DB_PASSWORD
+      : process.env.DB_PASSWORD,
+  database:
+    process.env.NODE_ENV === 'development'
+      ? process.env.DEVELOPMENT_DB_DATABASE
+      : process.env.DB_DATABASE,
   entities: [Users, Comments, CommentsRelations, Articles],
   synchronize: false,
   charset: 'utf8mb4',
