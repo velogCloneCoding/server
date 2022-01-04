65db4117d4013ac050dc640ccca9b9f213273884
8471919 <7471919@naver.com>
2022-01-04 18:19:15 +0900

    chore: 일단 들은 조언을 커밋 해두겠습니다. #0

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,11 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
+  CreateDateColumn,
+  DeleteDateColumn,
   Entity,
   Index,
   JoinColumn,
+  JoinTable,
+  ManyToMany,
   OneToMany,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 import { Articles } from '../../articles/entities/article.entity';
 import { Comments } from '../../comments/entities/comment.entity';
@@ -32,31 +37,6 @@ export class Users {
   githubProfile: string | null;
 
   @ApiProperty({
-    description: '회원가입 날짜',
-    example: '2022-02-22 22:22:22',
-  })
-  @Column('timestamp', {
-    name: 'CREATED_AT',
-    nullable: true,
-    default: () => 'CURRENT_TIMESTAMP',
-  })
-  createdAt: Date | null;
-
-  @ApiProperty({
-    description: '최근 유저정보 수정 날짜',
-    example: '2022-02-22 22:22:22',
-  })
-  @Column('timestamp', { name: 'UPDATED_AT', nullable: true })
-  updatedAt: Date | null;
-
-  @ApiProperty({
-    description: '회원탈퇴 날짜',
-    example: '2022-02-22 22:22:22',
-  })
-  @Column('timestamp', { name: 'DELETED_AT', nullable: true })
-  deletedAt: Date | null;
-
-  @ApiProperty({
     description: '이메일',
     example: 'example@gmail.com',
   })
@@ -75,11 +55,38 @@ export class Users {
   })
   password: string | null;
 
+  @ApiProperty({
+    description: '회원가입 날짜',
+    example: '2022-02-22 22:22:22',
+  })
+  @CreateDateColumn({ name: 'CREATED_AT' })
+  createdAt: Date | null;
+
+  @ApiProperty({
+    description: '최근 유저정보 수정 날짜',
+    example: '2022-02-22 22:22:22',
+  })
+  @UpdateDateColumn({ name: 'UPDATED_AT' })
+  updatedAt: Date | null;
+
+  @ApiProperty({
+    description: '회원탈퇴 날짜',
+    example: '2022-02-22 22:22:22',
+  })
+  @DeleteDateColumn({ name: 'DELETED_AT' })
+  deletedAt: Date | null;
+
   @OneToMany(() => Articles, (articles) => articles.users)
-  @JoinColumn()
   articles: Articles[];
 
   @OneToMany(() => Comments, (comments) => comments.users)
-  @JoinColumn()
   comments: Comments[];
+
+  // @ManyToMany(() => Desk, (desk) => desk.users)
+  // @JoinTable({
+  //   name: 'user_has_desks', // 테이블 이름
+  //   joinColumn: { name: 'user_id', referencedColumnName: 'idx' },
+  //   inverseJoinColumn: { name: 'desk_id', referencedColumnName: 'idx' },
+  // })
+  // desks: Desk[];
 }
