4a0543378c6d0d969f9f1260e806ca94d4c9e086
8471919 <7471919@naver.com>
2022-01-05 18:01:48 +0900

    fix: Comments 테이블 칼럼명 수정 #17

--- src/entities/comment.entity.ts
+++ src/entities/comment.entity.ts
@@ -12,47 +12,47 @@ import { Articles } from './article.entity';
 import { Users } from './user.entity';
 import { CommentsRelations } from '../comments-relations/entities/comments-relation.entity';
 
-@Index('fk_COMMENTS_ARTICLES1_idx', ['articlesId'], {})
-@Index('fk_COMMENTS_USERS1_idx', ['usersId'], {})
+@Index('fk_COMMENTS_ARTICLES1_idx', ['articleId'], {})
+@Index('fk_COMMENTS_USERS1_idx', ['userId'], {})
 @Entity('COMMENTS', { schema: 'velog' })
 export class Comments {
-  @PrimaryGeneratedColumn({ type: 'int', name: 'ID' })
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
   id: number;
 
-  @Column('longtext', { name: 'CONTENTS' })
+  @Column('longtext', { name: 'contents' })
   contents: string;
 
   @Column('timestamp', {
-    name: 'CREATED_AT',
+    name: 'created_at',
     nullable: true,
     default: () => 'CURRENT_TIMESTAMP',
   })
   createdAt: Date | null;
 
-  @Column('timestamp', { name: 'UPDATED_AT', nullable: true })
+  @Column('timestamp', { name: 'updated_at', nullable: true })
   updatedAt: Date | null;
 
-  @Column('timestamp', { name: 'DELETED_AT', nullable: true })
+  @Column('timestamp', { name: 'deleted_at', nullable: true })
   deletedAt: Date | null;
 
-  @Column('int', { name: 'ARTICLES_ID' })
-  articlesId: number;
+  @Column('int', { name: 'article_id' })
+  articleId: number;
 
-  @Column('int', { name: 'USERS_ID' })
-  usersId: number;
+  @Column('int', { name: 'user_id' })
+  userId: number;
 
   @ManyToOne(() => Articles, (articles) => articles.comments, {
     onDelete: 'NO ACTION',
     onUpdate: 'NO ACTION',
   })
-  @JoinColumn([{ name: 'ARTICLES_ID', referencedColumnName: 'id' }])
+  @JoinColumn([{ name: 'article_id', referencedColumnName: 'id' }])
   articles: Articles;
 
   @ManyToOne(() => Users, (users) => users.comments, {
     onDelete: 'NO ACTION',
     onUpdate: 'NO ACTION',
   })
-  @JoinColumn([{ name: 'USERS_ID', referencedColumnName: 'id' }])
+  @JoinColumn([{ name: 'user_id', referencedColumnName: 'id' }])
   users: Users;
 
   @OneToMany(
