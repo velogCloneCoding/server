56d76129075432b1cf7e07e9615671302cea3525
8471919 <7471919@naver.com>
2022-01-13 17:58:27 +0900

    fix: webpack에서는 glob경로의 사용이 불가능하여 autoLoadEntities 옵션을 추가 #18

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './res/users/users.module';
 import { ArticlesModule } from './res/articles/articles.module';
 import { CommentsModule } from './res/comments/comments.module';
-import { CommentsRelationsModule } from './comments-relations/comments-relations.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { Users } from './entities/user.entity';
-import { Comments } from './entities/comment.entity';
-import { CommentsRelations } from './comments-relations/entities/comments-relation.entity';
-import { Articles } from './entities/article.entity';
 
 import * as dotenv from 'dotenv';
 import { AuthModule } from './auth/auth.module';
@@ -25,16 +19,15 @@ dotenv.config();
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [Users, Comments, CommentsRelations, Articles],
       dateStrings: true,
       synchronize: false,
       logging: true,
+      autoLoadEntities: true,
     }),
     AuthModule,
     UsersModule,
     ArticlesModule,
     CommentsModule,
-    CommentsRelationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
