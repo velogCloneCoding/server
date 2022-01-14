370e2cccbc903613d1f4f264a7207a48994e1f15
8471919 <7471919@naver.com>
2022-01-14 18:59:01 +0900

    feat: ConfigModule 설정 완료 #19

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,27 +4,27 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const ORMConfig: any & ConnectionOptions = {
-  type: 'mysql',
+  type: process.env.DB_TYPE,
   host:
-    process.env.NODE_ENV === 'development'
+    process.env.NODE_ENV === 'DEVELOPMENT'
       ? process.env.DEVELOPMENT_DB_HOST
-      : process.env.TEST_DB_HOST || process.env.DB_HOST,
+      : process.env.PRODUCT_DB_HOST,
   port:
-    process.env.NODE_ENV === 'development'
-      ? process.env.DEVELOPMENT_DB_PORT
-      : Number(process.env.DB_PORT),
+    process.env.NODE_ENV === 'DEVELOPMENT'
+      ? Number(process.env.DEVELOPMENT_DB_PORT)
+      : Number(process.env.PRODUCT_DB_PORT),
   username:
-    process.env.NODE_ENV === 'development'
+    process.env.NODE_ENV === 'DEVELOPMENT'
       ? process.env.DEVELOPMENT_DB_USERNAME
-      : process.env.DB_USERNAME,
+      : process.env.PRODUCT_DB_USERNAME,
   password:
-    process.env.NODE_ENV === 'development'
+    process.env.NODE_ENV === 'DEVELOPMENT'
       ? process.env.DEVELOPMENT_DB_PASSWORD
-      : process.env.DB_PASSWORD,
+      : process.env.PRODUCT_DB_PASSWORD,
   database:
-    process.env.NODE_ENV === 'development'
+    process.env.NODE_ENV === 'DEVELOPMENT'
       ? process.env.DEVELOPMENT_DB_DATABASE
-      : process.env.DB_DATABASE,
+      : process.env.PRODUCT_DB_DATABASE,
   entities: ['src/entities/**/*.ts'],
   synchronize: false,
   // dateStrings: true,
