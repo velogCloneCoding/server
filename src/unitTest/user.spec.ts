badd507de78a13f3effca787da2397ab859680cd
8471919 <7471919@naver.com>
2022-01-16 20:05:40 +0900

    test: 일부 테스트에 대한 skeleton 작성 #23

--- src/unitTest/user.spec.ts
+++ src/unitTest/user.spec.ts
@@ -67,7 +67,7 @@ describe('사용자에 대한 테스트입니다.', () => {
     commentsService = await app.resolve<CommentsService>(CommentsService);
   });
 
-  describe('0. 테스트 준비', () => {
+  describe('0-. 테스트 준비', () => {
     it('0-1. Users의 Controller와 Service가 주입되었는지 확인합니다.', () => {
       expect(usersController).toBeDefined();
       expect(usersService).toBeDefined();
@@ -82,5 +82,64 @@ describe('사용자에 대한 테스트입니다.', () => {
       expect(commentsController).toBeDefined();
       expect(commentsService).toBeDefined();
     });
+  });
+
+  describe('1-. Users 로직 테스트', () => {
+    describe('1-1. Users 회원가입', () => {});
+
+    describe('1-2. Users 내 정보 조회', () => {});
+
+    describe('1-3. Users 내 정보 수정', () => {});
+
+    describe('1-4. Users 회원탈퇴', () => {});
+  });
+
+  describe('2-. Articles 로직 테스트', () => {
+    describe('2-1. Articles 작성', () => {});
+
+    describe('2-2. Articles 수정', () => {});
+
+    describe('2-3. Articles 삭제', () => {});
+
+    describe('2-4. Articles 목록 조회', () => {
+      it.todo('2-4-1. 페이지별로 조회합니다.', async () => {});
+      it.todo('2-4-2. hashtag별로 조회합니다.', async () => {});
+    });
+
+    describe('2-5. Articles 내용 조회', () => {});
+  });
+
+  describe('3-. Comments 로직 테스트', () => {
+    describe('3-1. Comments 작성', () => {});
+
+    describe('3-2. Comments 수정', () => {});
+
+    describe('3-3. Comments 삭제', () => {});
+
+    describe('3-4. Comments 조회', () => {
+      it.todo('3-4-1. 작성자별로 댓글을 조회합니다.', () => {});
+    });
+  });
+
+  describe('4-. SeriesArticles 로직 테스트', () => {
+    describe('4-1. SeriesArticles 작성', () => {});
+
+    describe('4-2. SeriesArticles 수정', () => {});
+
+    describe('4-3. SeriesArticles 삭제', () => {});
+
+    describe('4-4. SeriesArticles 조회', () => {});
+
+    describe('4-5. SeriesArticles에 게시글 추가/삭제', () => {});
+  });
+
+  describe('5-. Introductions 로직 테스트', () => {
+    describe('5-1. Introductions 수정', () => {});
+  });
+
+  describe('6-. HashTags 로직 테스트', () => {
+    describe('6-1. HashTags 생성', () => {});
+
+    describe('6-2. HashTags를 사용순으로 검색', () => {});
   });
 });
