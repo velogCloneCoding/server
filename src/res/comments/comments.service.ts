ba4303656fd4244d2c4258f8f970a73bee27c837
8471919 <7471919@naver.com>
2022-01-20 16:35:00 +0900

    chore: repository injection #25

--- src/res/comments/comments.service.ts
+++ src/res/comments/comments.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comments } from 'src/entities/comment.entity';
+import { Repository } from 'typeorm';
 import { CreateCommentDto } from '../../dto/create-comment.dto';
 import { UpdateCommentDto } from '../../dto/update-comment.dto';
 
 @Injectable()
 export class CommentsService {
+  constructor(
+    @InjectRepository(Comments)
+    private readonly commentsRepository: Repository<Comments>,
+  ) {}
   create(createCommentDto: CreateCommentDto) {
     return 'This action adds a new comment';
   }
