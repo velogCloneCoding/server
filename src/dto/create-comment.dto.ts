abbb0fe337b00ed7ca1a01160c680b6f824ef0e7
8471919 <7471919@naver.com>
2022-01-20 18:01:29 +0900

    chore: create-comment-dto 생성 #25

--- src/dto/create-comment.dto.ts
+++ src/dto/create-comment.dto.ts
@@ -1,1 +1,8 @@
-export class CreateCommentDto {}
+import { PickType } from '@nestjs/swagger';
+import { Comments } from 'src/entities/comment.entity';
+
+export class CreateCommentDto extends PickType(Comments, [
+  'contents',
+  'articleId',
+  'parentId',
+] as const) {}
