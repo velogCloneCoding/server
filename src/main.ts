679c575b420599fb4d7e2a259cca33d705b0ff74
8471919 <7471919@naver.com>
2022-02-07 20:05:18 +0900

    chore: main.ts에 useGlobalPipe(ValidationPipe) 추가 #0

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -9,6 +10,8 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
 
   // app.useGlobalInterceptors(new TransformInterceptor());
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .addBearerAuth()
